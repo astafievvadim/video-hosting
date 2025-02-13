4ee462cc5c032a144428303815933b19f6d8ab98
ThatPotato <gamelikecar@gmail.com>
2025-02-14 02:41:08 +0400

    tested multiple services, everything works

--- services/user-service/src/main/java/com/astafievvadim/userservice/user/User.java
+++ services/user-service/src/main/java/com/astafievvadim/userservice/user/User.java
@@ -1,11 +1,6 @@
 package com.astafievvadim.userservice.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
